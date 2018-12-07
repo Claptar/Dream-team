f06e73f118db6f0d6cb558c63db83ec871cc7b7c
MaKYaro <klykin-2@yandex.ru>
2018-12-07 13:22:30 +0300

    [артиллерия] Создан класс AI

---
+++ BOT_Challenge/AI.py
@@ -0,0 +1,33 @@
+import cannon
+class AI:
+    def __init__(self):
+       pass
+
+    def create(self):
+        """
+        Создает объект пушки
+        :return:
+        """
+        cannon = Cannon()
+
+    def aim(self, cannon.x, :
+        """
+        Направляет дуло пушки
+        :return:
+        :param x, y: координаты пушки противника
+        """
+
+
+    def fire(self):
+        """
+        Запускает ядро, учитывая координаты пушки противника
+        и предыдушие результаты выстрелов
+        :return:
+        """
+
+    def check_hit(self, x, y):
+        """
+        Получает координаты попадания ядра
+        :param x, y: координаты попадания ядра
+        :return:
+        """
