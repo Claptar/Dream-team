6b79d1633155c9732c264618bce5e0ff85fdc2fa
Claptar <aleksandr.binkevich@gmail.com>
2018-12-13 01:28:53 +0300

    [артилерия] Изменил пределы рандомного размещения пушки

--- BOT_Challenge/Canon_generator.py
+++ BOT_Challenge/Canon_generator.py
@@ -8,7 +8,7 @@ import Canon
 
 
 def find_place():
-    x = random.randint(0, 100)
+    x = random.randint(0, 300)
     y = 0
     while Landskape.img.get(x, y) != Landskape.check_color:
         y += 1
