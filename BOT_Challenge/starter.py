8220f81ba1efb62ac388854e40002f58ccd668d1
Claptar <aleksandr.binkevich@gmail.com>
2018-12-13 01:29:55 +0300

    [артилерия] Добавил полноэкранный режим и генерацию пушки в рандомной точке в зависимости от ландшафта

--- BOT_Challenge/starter.py
+++ BOT_Challenge/starter.py
@@ -1,14 +1,16 @@
 import math
 import random
-from ball_class import *
 from tkinter import *
 import Canon
 import Canon_generator as cg
 root1 = Canon.root
+root1.overrideredirect(True)
+root1.overrideredirect(False)
+root1.attributes('-fullscreen', True)
 Canon.canv = Canvas(root1, width=root1.winfo_screenwidth(), height=root1.winfo_screenheight(), bg='white')
 canvas = Canon.canv
 canvas.pack(fill=BOTH, expand=1)
-cannon = Canon.Cannon(canvas, 30, 600)
+cannon = cg.create(canvas)
 Canon.cannon = cannon
 canvas.bind('<Motion>', Canon.mouse_move_handler)
 canvas.bind("<ButtonPress-1>", Canon.time_start)
