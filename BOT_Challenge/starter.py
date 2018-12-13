761eab88990fa49c818ab7116757580f7a42c39a
Claptar <aleksandr.binkevich@gmail.com>
2018-12-14 01:24:15 +0300

    [артилерия] -Добавил генерацию пушки бота и возможность загрузки картинки

--- BOT_Challenge/starter.py
+++ BOT_Challenge/starter.py
@@ -3,14 +3,18 @@ import random
 from tkinter import *
 import Canon
 import Canon_generator as cg
+from PIL import Image, ImageTk
 root1 = Canon.root
 root1.overrideredirect(True)
 root1.overrideredirect(False)
 root1.attributes('-fullscreen', True)
-Canon.canv = Canvas(root1, width=root1.winfo_screenwidth(), height=root1.winfo_screenheight(), bg='white')
 canvas = Canon.canv
 canvas.pack(fill=BOTH, expand=1)
+
+ball_image = ImageTk.PhotoImage(Image.open('1.png'))
+#canvas.create_image(ball_image.size[0], root1.winfo_screenheight()/2, image=ball_image)
 cannon = cg.create(canvas)
+bot_cannon = Canon.Cannon(canvas, 500, 600)
 Canon.cannon = cannon
 canvas.bind('<Motion>', Canon.mouse_move_handler)
 canvas.bind("<ButtonPress-1>", Canon.time_start)
