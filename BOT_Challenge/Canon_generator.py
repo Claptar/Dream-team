ae7dbe23fd274f8f3eca50e45796cfc29f4f83dc
Claptar <aleksandr.binkevich@gmail.com>
2018-12-17 01:19:56 +0300

    [артиллерия] - Добавил выбор пределов расположения пушек при их генерации

--- BOT_Challenge/Canon_generator.py
+++ BOT_Challenge/Canon_generator.py
@@ -7,15 +7,15 @@ import Landskape
 import Canon
 
 
-def find_place():
-    x = random.randint(0, 600)
+def find_place(x_start, x_end):
+    x = random.randint(x_start, x_end)
     y = 0
     while Landskape.img.get(x, y) != Landskape.check_color:
         y += 1
     return [x, y]
 
 
-def create(canvas):
-    coordinates = find_place()
+def create(canvas, x_start, x_end):
+    coordinates = find_place(x_start, x_end)
     return Canon.Cannon(canvas, coordinates[0], coordinates[1])
 
